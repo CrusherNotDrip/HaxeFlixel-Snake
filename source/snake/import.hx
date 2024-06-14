1fbd0f695296edfe67db3eb375644333a35f55a8
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-06-14 05:15:48 -0400

    Constants created

---
+++ source/snake/import.hx
@@ -0,0 +1,1 @@
+import snake.utils.Constants;
---
+++ source/snake/utils/Constants.hx
@@ -0,0 +1,12 @@
+package snake.utils;
+
+/**
+ * A class full of final variables.
+ */
+class Constants
+{
+	/**
+	 * This defines what size the objects are in each square.
+	 */
+	public static final OBJECT_SIZE:Float = 20;
+}
